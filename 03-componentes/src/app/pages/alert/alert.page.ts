bedadc2258306e134c6ff65d67be18b9920a5366
Mateo Ortiz Cano - Ceiba Software <mateo.ortiz@ceiba.com.co>
2020-08-06 11:28:47 -0500

    Ejemplo de ion-action

--- 03-componentes/src/app/pages/alert/alert.page.ts
+++ 03-componentes/src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  titulo: string = 'alert';
+
+  constructor(private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: (blah) => {
+          console.log('Cancelar');
+        }
+      },
+      {
+        text: 'Ok',
+        cssClass: 'secondary',
+        handler: (blah) => {
+          console.log('Botón ok');
+        }
+      }]
+    });
+
+    await alert.present();
+  }
+
+  async presentInputAlert() {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Ingrese nombre'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            this.titulo = data.name1;
+            console.log('OK', data);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
