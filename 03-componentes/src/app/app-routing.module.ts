e9f50cbd2a296e4a48a03189bacc97de89ce9aa0
Mateo Ortiz Cano - Ceiba Software <mateo.ortiz@ceiba.com.co>
2020-08-07 10:39:35 -0500

    Ejemplo con botones

--- 03-componentes/src/app/app-routing.module.ts
+++ 03-componentes/src/app/app-routing.module.ts
@@ -27,6 +27,10 @@ const routes: Routes = [
     path: 'avatar',
     loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule)
   },
+  {
+    path: 'buttons',
+    loadChildren: () => import('./pages/buttons/buttons.module').then( m => m.ButtonsPageModule)
+  },
 ];
 
 @NgModule({
--- 03-componentes/src/app/pages/inicio/inicio.page.ts
+++ 03-componentes/src/app/pages/inicio/inicio.page.ts
@@ -22,6 +22,11 @@ export class InicioPage implements OnInit {
       icon: 'beaker',
       name: 'Avatar',
       redirectTo: '/avatar'
+    },
+    {
+      icon: 'radio-button-on-outline',
+      name: 'Button',
+      redirectTo: '/buttons'
     }
   ];
 
