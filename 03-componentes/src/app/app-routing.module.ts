755120b5d81cdbb6139311f6dd375dd546b98eb3
Mateo Ortiz Cano - Ceiba Software <mateo.ortiz@ceiba.com.co>
2020-08-17 13:57:55 -0500

    agrego ejemplo con split pane para aprovechar las pantallas grandes y mostrar mas contenido dividido en ellas

--- 03-componentes/src/app/app-routing.module.ts
+++ 03-componentes/src/app/app-routing.module.ts
@@ -77,27 +77,27 @@ const routes: Routes = [
   },
   {
     path: 'popover',
-    loadChildren: () => import('./pages/popover/popover.module').then( m => m.PopoverPageModule)
+    loadChildren: () => import('./pages/popover/popover.module').then(m => m.PopoverPageModule)
   },
   {
     path: 'progress-bar',
-    loadChildren: () => import('./pages/progress-bar/progress-bar.module').then( m => m.ProgressBarPageModule)
+    loadChildren: () => import('./pages/progress-bar/progress-bar.module').then(m => m.ProgressBarPageModule)
   },
   {
     path: 'refresher',
-    loadChildren: () => import('./pages/refresher/refresher.module').then( m => m.RefresherPageModule)
+    loadChildren: () => import('./pages/refresher/refresher.module').then(m => m.RefresherPageModule)
   },
   {
     path: 'searchbar',
-    loadChildren: () => import('./pages/searchbar/searchbar.module').then( m => m.SearchbarPageModule)
+    loadChildren: () => import('./pages/searchbar/searchbar.module').then(m => m.SearchbarPageModule)
   },
   {
     path: 'segment',
-    loadChildren: () => import('./pages/segment/segment.module').then( m => m.SegmentPageModule)
+    loadChildren: () => import('./pages/segment/segment.module').then(m => m.SegmentPageModule)
   },
   {
     path: 'slides',
-    loadChildren: () => import('./pages/slides/slides.module').then( m => m.SlidesPageModule)
+    loadChildren: () => import('./pages/slides/slides.module').then(m => m.SlidesPageModule)
   },
 ];
 
--- 03-componentes/src/app/app.component.ts
+++ 03-componentes/src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Observable } from 'rxjs';
+import { Componentt } from './interfaces/interfaces';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +13,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  components: Observable<Componentt[]>;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private dataService: DataService
   ) {
     this.initializeApp();
   }
@@ -22,6 +29,7 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.components = this.dataService.getMenuOptions();
     });
   }
 }
