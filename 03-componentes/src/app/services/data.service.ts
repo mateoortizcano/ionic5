085713098c84f17587e34114349e9609975f6afb
Mateo Ortiz Cano - Ceiba Software <mateo.ortiz@ceiba.com.co>
2020-08-17 13:17:21 -0500

    agrego ejemplo de skeleton: formas (sombras) del contenido que aparecen mientras el contenido aparece

--- 03-componentes/src/app/services/data.service.ts
+++ 03-componentes/src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Componentt } from '../interfaces/interfaces';
+import { delay } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +23,8 @@ export class DataService {
   }
 
   getHeroes() {
-    return this.http.get<any[]>('/assets/data/superheroes.json');
+    return this.http.get<any[]>('/assets/data/superheroes.json').pipe(
+      delay(1500)
+    );
   }
 }
