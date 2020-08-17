db10ade1cd0eba4c25a19f206593ef8ce4e4ed7e
Mateo Ortiz Cano - Ceiba Software <mateo.ortiz@ceiba.com.co>
2020-08-17 14:17:58 -0500

    agrego ejemplo con toast: muestra mensajes notificación dentro de la app

--- 03-componentes/src/app/pages/list/list.page.ts
+++ 03-componentes/src/app/pages/list/list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Observable } from 'rxjs';
-import { IonList } from '@ionic/angular';
+import { IonList, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -12,23 +12,32 @@ export class ListPage implements OnInit {
 
   @ViewChild('lista') lista: IonList;
   usuarios: Observable<any>;
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+    private toastCtrl: ToastController) { }
 
   ngOnInit() {
     this.usuarios = this.dataService.getUsers();
   }
 
   favorite(user) {
-    console.log('favourite:', user);
+    this.presentToast('Guardó favoritos.')
     this.lista.closeSlidingItems();
   }
   share(user) {
-    console.log('share:', user);
+    this.presentToast('Compartido!');
     this.lista.closeSlidingItems();
   }
   delete(user) {
-    console.log('delete', user);
+    this.presentToast('Se eliminó.')
     this.lista.closeSlidingItems();
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
