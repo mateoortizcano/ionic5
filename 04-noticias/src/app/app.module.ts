59a650138b8350799244c6c017e2b90f9f27c06d
Mateo Ortiz Cano - Ceiba Software <mateo.ortiz@ceiba.com.co>
2020-09-01 10:35:01 -0500

    agrego funcion para que la noticia se abra en el navegador del dispositivo

--- 04-noticias/src/app/app.module.ts
+++ 04-noticias/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http'
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,6 +23,7 @@ import { HttpClientModule } from '@angular/common/http'
   providers: [
     StatusBar,
     SplashScreen,
+    InAppBrowser,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- 04-noticias/src/app/components/noticia/noticia.component.ts
+++ 04-noticias/src/app/components/noticia/noticia.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-noticia',
@@ -10,8 +11,12 @@ export class NoticiaComponent implements OnInit {
 
   @Input() noticia: Article;
   @Input() indice: number;
-  constructor() { }
+  constructor(private iab: InAppBrowser) { }
 
   ngOnInit() { }
+
+  abrirNoticia() {
+    const browser = this.iab.create(this.noticia.url, '_system');
+  }
 
 }
